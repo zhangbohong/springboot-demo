fc837f8447d0344af57bd93f89a2a271171fda83
zhangbohong <zhangbh_416@163.com>
2019-03-19 23:26:57 +0800

    修改导包

--- src/main/java/com/myproject/service/UserService.java
+++ src/main/java/com/myproject/service/UserService.java
@@ -16,23 +16,19 @@ public class UserService {
 
     @TargetDataSource(TargetDataSource.master)
     public List<User> getAllUser() {
-        // TODO 自动生成的方法存根
         return iUserMapper.getAllUser();
     }
 
     @TargetDataSource(TargetDataSource.readonly)
     public User getUserByID(int id) {
-        // TODO 自动生成的方法存根
         return iUserMapper.getUserByID(id);
     }
 
     public int deleteUserById(int id) {
-        // TODO 自动生成的方法存根
         return iUserMapper.deleteUserById(id);
     }
 
     public int updateUser(User user) {
-        // TODO 自动生成的方法存根
         return iUserMapper.updateUser(user);
     }
 }
